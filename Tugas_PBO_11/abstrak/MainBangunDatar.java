ce428e7c22cade2f855517a95082db8dd3966130
nanda <najibfina02@gmail.com>
2026-06-06 19:58:10 +0700

    tugas 11

--- Tugas_PBO_11/abstrak/MainBangunDatar.java
+++ Tugas_PBO_11/abstrak/MainBangunDatar.java
@@ -3,7 +3,7 @@ package abstrak;
 public class MainBangunDatar {
     public static void main(String[] args) {
         Kotak kotakSaya = new Kotak(6, 4);
-        System.out.println("=== OBJEK KOTAK ===");
+        System.out.println("KOTAK");
         System.out.println("Jumlah Sisi: " + kotakSaya.getJumlahSisi());
         System.out.println("Luas Kotak: " + kotakSaya.getLuas());
         System.out.println("Keliling Kotak: " + kotakSaya.getKeliling());
@@ -12,7 +12,7 @@ public class MainBangunDatar {
         System.out.println();
 
         Segitiga segitigaSaya = new Segitiga(6, 8);
-        System.out.println("=== OBJEK SEGITIGA ===");
+        System.out.println("SEGITIGA");
         System.out.println("Jumlah Sisi: " + segitigaSaya.getJumlahSisi());
         System.out.println("Luas Segitiga: " + segitigaSaya.getLuas());
         System.out.println("Keliling Segitiga: " + segitigaSaya.getKeliling());
--- Tugas_PBO_11/abstrak/MainInterface.java
+++ Tugas_PBO_11/abstrak/MainInterface.java
@@ -4,13 +4,13 @@ public class MainInterface {
     public static void main(String[] args) {
         Kotak kotakSaya = new Kotak(4, 5);
 
-        System.out.println("=== SEBELUM RESIZE ===");
+        System.out.println("SEBELUM RESIZE");
         System.out.println("Luas Kotak: " + kotakSaya.getLuas());
         System.out.println("Keliling Kotak: " + kotakSaya.getKeliling());
 
         kotakSaya.resize(2.0);
 
-        System.out.println("\n=== SETELAH RESIZE 2X ===");
+        System.out.println("\nSETELAH RESIZE");
         System.out.println("Luas Kotak: " + kotakSaya.getLuas());
         System.out.println("Keliling Kotak: " + kotakSaya.getKeliling());
     }
